5ab399038bffcd7cebc6ce6c1d54d23380356c31
6xiaozhou <89899123+6xiaozhou@users.noreply.github.com>
2022-08-12 11:26:17 +0800

    Create GroupSigPrecompiled.sol

---
+++ GroupSigPrecompiled.sol
@@ -0,0 +1,5 @@
+// GroupSigPrecompiled.sol
+pragma solidity ^0.4.24;
+contract GroupSigPrecompiled{
+		function groupSigVerify(string signature, string message, string gpkInfo, string paramInfo) public constant returns(bool);
+}
